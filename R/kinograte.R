995a2a56593006e3c0c184e37d0213a9b66a10c5
kalganem <khaled.a.alganem@gmail.com>
2022-04-15 01:05:08 -0400

    add clustering options

--- R/kinograte.R
+++ R/kinograte.R
@@ -4,20 +4,20 @@
 #'
 #' @param df combined ranked omic datasets
 #' @param ppi_network dataframe of protein-protein interactions
-#' @param seed (optional) set seed number
 #' @param n An integer value to determine the number of runs with random noise added edge costs. A default value is 10.
 #' @param w A numeric value for tuning the number of trees in the output. A default value is 2.
 #' @param r A numeric value to determine additional random noise to edge costs. A random noise upto r percent of the edge cost is added to each edge. A default value is 0.1
 #' @param b A numeric value for tuning the node prizes. A default value is 1.
 #' @param mu A numeric value for a hub penalization. A default value is 0.0005.
-#'
+#' @param cluster set TRUE to cluster the network
+#' @param seed (optional) set seed number
 #' @return list(network, nodes, edges)
 #' 
 #' @export
 #'
 
 
-kinograte <- function(df, ppi_network, n = 10, w = 10, r = 0.1, b = 2, mu = 0.005,seed = NULL) {
+kinograte <- function(df, ppi_network, n = 10, w = 10, r = 0.1, b = 2, mu = 0.005, cluster = TRUE, seed = NULL) {
   
   
   terms <- df$prize
@@ -45,13 +45,36 @@ kinograte <- function(df, ppi_network, n = 10, w = 10, r = 0.1, b = 2, mu = 0.00
     dplyr::mutate(type = ifelse(is.na(type), "Hidden", type), 
            prize = ifelse(is.na(prize), 2, prize)) -> nodes
   
-  
-  return(
-    list(
-      network = subnet,
-      nodes = nodes,
-      edges = edges
+  if(cluster) {
+    wc <- suppressWarnings(igraph::edge.betweenness.community(subnet, 
+                                                              weights = igraph::E(subnet)$weight, 
+                                                              directed = FALSE, 
+                                                              bridges=TRUE))
+    
+    wc_df <- dplyr::tibble(
+      id = wc$names,
+      cluster = wc$membership
     )
-  )
+    
+    return(
+      list(
+        network = subnet,
+        nodes = nodes,
+        edges = edges,
+        wc_df = wc_df
+      )
+    )
+    
+  }
+  
+  else {
+    return(
+      list(
+        network = subnet,
+        nodes = nodes,
+        edges = edges
+        )
+    )
+    }
   
 }
